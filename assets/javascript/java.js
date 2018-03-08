c76d3bc2eb28b6384a817fb7fb14e521da036da7
Ross Messer <rmesser@perennial.local>
2018-03-07 21:36:40 -0600

    bug fix

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 
     function displayGif() {
-        $("#gifs-view").empty();
+        $("#gifs-viewer").empty();
         var searchGif = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=j8LBUB49P4obbRWYVF2YI9emqXQImNmk&tag=" + searchGif;
+
+        var apiKey = "j8LBUB49P4obbRWYVF2YI9emqXQImNmk";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchGif + "&limit=25&offset=0&rating=G&lang=en";
         console.log(queryURL)
         // Creating an AJAX call for the specific movie button being clicked
         function ajaxSearch() {
@@ -12,57 +14,67 @@ $(document).ready(function () {
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
-                console.log(response.data.rating);
+                console.log(response.data[0].rating);
+                console.log(response.data[0].title);
+                console.log(response.data[0].images.fixed_height_small.url);
                 // Creating a div to hold the movie
-                var topicDiv = $("<div class='unit' data-state='still'>").addClass("topic");
+                for (var i = 0; i < 10; i++) {
+                    function renderer() {
+                        var topicDiv = $("<div class='unit'>").addClass("topic");
+                        // Storing the rating data
+                        var rating = response.data[i].rating;
 
-                // Storing the rating data
-                var rating = response.data.rating;
+                        // Creating an element to have the rating displayed
+                        var pOne = $("<p>").text("Rating: " + rating);
 
-                // Creating an element to have the rating displayed
-                var pOne = $("<p>").text("Rating: " + rating);
+                        // Displaying the rating
+                        topicDiv.append(pOne);
+                        //   // Retrieving the URL for the image
+                        // var stillImgURL = response.data.fixed_height_small_still_url;
 
-                // Displaying the rating
-                topicDiv.append(pOne);
-                //   // Retrieving the URL for the image
-                // var stillImgURL = response.data.fixed_height_small_still_url;
+                        // var motionImgUrl = response.data.fixed_height_small_url;
 
-                // var motionImgUrl = response.data.fixed_height_small_url;
+                        //   // Creating an element to hold the image
+                        var image = $("<img class='gif'>");
+                        image.attr("src", response.data[i].images.fixed_height_small_still.url);
+                        image.attr({ 'data-animate': response.data[i].images.fixed_height_small.url });
+                        image.attr({ 'data-state': "still" });
+                        image.attr({ 'data-still': response.data[i].images.fixed_height_small_still.url });
 
-                //   // Creating an element to hold the image
-                var image = $("<img id='gifImg'>");
-                image.attr("src", response.data.fixed_height_small_still_url);
-                image.attr({ 'data-animate': response.data.fixed_height_small_url });
-                image.attr({ 'data-state': "still" });
-                image.attr({ 'data-still': response.data.fixed_height_small_still_url });
+                        //   // Appending the image
+                        topicDiv.append(image);
 
-                //   // Appending the image
-                topicDiv.append(image);
-
-                // Putting the entire movie above the previous movies
-                $("#gifs-view").append(topicDiv);
+                        // Putting the entire movie above the previous movies
+                        $("#gifs-viewer").append(topicDiv);
+                    }
+                    for (var i = 0; i < 10; i++) {
+                    renderer()
+                    };
+                }
 
             });
         }
-        for (var i = 0; i < 10; i++) {
-            ajaxSearch()
-        }
-
-        $("#gifImg").on("click", function () {
-
-
-            var state = $(this).attr('data-state');
-
-
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }
-        });
+        ajaxSearch()
     }
+
+    // $(document).on("click", ".movie-btn", displayMovieInfo);
+
+    $(document).on("click", ".gif", function () {
+        console.log("This is working");
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+        
+    });
 
     var topics = ["bigfoot", "chupacabra", "nessie", "megalodon", "yeti", "yowie", "kraken"];
     function renderButtons() {
@@ -76,6 +88,7 @@ $(document).ready(function () {
         }
     }
     $("#add-topic").on("click", function (event) {
+        console.log("GOT IT");
         event.preventDefault();
         var topic = $("#topic-input").val().trim();
         topics.push(topic);
